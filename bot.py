060fcf262731e882b196b380b7eef070ebb83597
AndyLeiva <andy.leiva.camacho@gmail.com>
2021-12-13 12:54:09 -0600

    classes actualizadas

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
-
+import time
 driver = webdriver.Chrome(executable_path='webdrivers\chromedriver.exe')
-driver.get('https://andyleiva.github.io/Chill-Place/')
+driver.get('https://andyleiva.github.io/applicacion-prueba/')
+
+
+driver.find_element_by_xpath('//*[@id="botonclick"]')
+driver.click()
