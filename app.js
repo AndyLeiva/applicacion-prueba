78135b2dc295af2df4a4a3bbbab8137c0b85ed0a
AndyLeiva <andy.leiva.camacho@gmail.com>
2021-12-13 12:35:49 -0600

    primer commit

---
+++ app.js
@@ -0,0 +1,33 @@
+
+
+function resizeImg(){
+let image = document.getElementById("image");
+image.style.height='350px';
+
+
+
+};
+
+function borderRadiusImg(){
+    let image = document.getElementById("image");
+    
+    image.style.borderRadius='5px';
+   
+    
+};
+
+function paddingImg(){
+    let image = document.getElementById("image");
+    
+    image.style.padding='100px';
+    
+};
+
+function restar(){
+    let image = document.getElementById("image");
+    
+    image.style.padding='0px';
+    image.style.borderRadius='0px'
+    image.style.height='190px'
+    
+};
---
+++ bot.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path='webdrivers\chromedriver.exe')
+driver.get('https://andyleiva.github.io/Chill-Place/')
